a5e156cb7a339b63c111003d12ce775de8f7ed65
Miles Stoudenmire <mstoudenmire@flatironinstitute.org>
2026-09-19 22:44:56 -0400

    Clean up 3rd tutorial

--- Tutorials/HandsOn3/3-2d-function.jl
+++ Tutorials/HandsOn3/3-2d-function.jl
@@ -1,59 +1,62 @@
-using ITensorMPS: siteinds, linkdims, maxlinkdim
+using ITensorMPS: siteinds, linkdims, maxlinkdim, orthogonalize, normalize, sample
 using Random: Random
-using Plots: contourf, plot!, RGB, mm
-
+using Plots: contourf, plot!, scatter!, RGB, mm
 
 include("resources/tensor_cross/tensor_cross.jl")
 include("resources/qtt_utils.jl")
 
-function main(; nbits = 16, random_seed=1)
+function main(; nbits = 16,  # number of bits per dimension for QTT encoding
+                # Parameters controlling f(x,y)
+                Ngaussian = 50, # number of Gaussians to sum to make f(x,y)
+                width = 0.01,  # typical width of each Gaussian
+                Nsamples = 1000, 
+                # Tensor cross related parameters
+                maxdim = 60,
+                cutoff = 1E-5,
+                nsweep = 5,
+                random_seed=1)
+
   Random.seed!(random_seed)
 
-  show_pivots = false
-
-
   #
-  # Sum of random Gaussians
+  # Define f(x,y) to be a
+  # sum of Ngaussian random Gaussians
+  # of typical width W
   #
-  Ng = 50
-  ω = 0.01
-  ws = [ω*rand() for g=1:Ng]
-  xcs = [rand() for g=1:Ng]
-  ycs = [rand() for g=1:Ng]
-  signs = [rand((+1,-1)) for g=1:Ng]
-  heights = [rand() for g=1:Ng]
+  ws = [width*rand() for g=1:Ngaussian]
+  xcs = [rand() for g=1:Ngaussian]
+  ycs = [rand() for g=1:Ngaussian]
+  signs = [rand((+1,-1)) for g=1:Ngaussian]
+  heights = [rand() for g=1:Ngaussian]
   function f(x,y)
     val = 0.0
-    for g=1:Ng
+    for g=1:Ngaussian
       val += heights[g]*signs[g]*exp(-((x-xcs[g])^2+(y-ycs[g])^2)/ws[g])
     end
     return val
   end
 
-
+  #
+  # Call tensor_cross to load the function
+  #
   println("Performing tensor cross interpolation:")
-  maxdim = 60
-  cutoff = 1E-5
-  nsweep = 5
-  M,info = tensor_cross(siteinds("Qubit",2*nbits),(i...)->f(b2c(i[1:nbits]...),b2c(i[nbits+1:end]...));
+  M, info = tensor_cross(siteinds("Qubit",2*nbits),(i...)->f(b2c(i[1:nbits]...),b2c(i[nbits+1:end]...));
                                nsweep, maxdim, cutoff, outputlevel=1)
   println("Bond dimensions χ's = \n",linkdims(M))
 
-  #N = 2^(2n)
-  #Nc = length(cache)
-  #@printf("\n%d queries versus %d grid points. %.8f%% of grid points used.\n",Nc,N,100*Nc/N)
-
-  #Nsample = 1000
-  #avg_err = 0.0
-  #Ms = copy(M)
-  #orthogonalize!(Ms,1)
-  #normalize!(Ms)
-  #for ns in 1:Nsample
-  #  sa = sample!(Ms)
-  #  x,y = b2c(sa[1:n]),b2c(sa[n+1:end])
-  #  avg_err += abs(f(x,y)-evaluate(M,sa))/Nsample
-  #end
-  #@printf("Average error (sampled) = %.4E\n",avg_err)
+  #
+  # Sample the MPS. Each sample is a vector of bits like [1,2,2,1,...]
+  # drawn with probability |M(bits)|^2. The first nbits are the
+  # bits of x and the remaining nbits are the bits of y.
+  #
+  Ms = normalize(orthogonalize(M,1))
+  sample_x = Float64[]
+  sample_y = Float64[]
+  for ns in 1:Nsamples
+    s = sample(Ms)
+    push!(sample_x, b2c(s[1:nbits]))
+    push!(sample_y, b2c(s[nbits+1:end]))
+  end
 
   # 
   # Plot
@@ -64,8 +67,15 @@ function main(; nbits = 16, random_seed=1)
 
   plt = contourf(x, y, z'; levels=20, color=:lajolla, linewidth=0,
                  aspect_ratio=:equal, xlims=(0,1), ylims=(0,1), size=(900,850),
-                 top_margin=10mm, legend=:outertop, legend_columns=2,
+                 top_margin=10mm, legend=:outertop, legend_columns=3,
                  legendfontsize=12, foreground_color_legend=nothing)
+
+  #
+  # Show location of each sample as a small blue point
+  #
+  bright_blue = RGB(0, 150/255, 1)
+  scatter!(plt, sample_x, sample_y; color=bright_blue, markersize=2.5,
+           markerstrokewidth=0, label="Samples")
 
   #
   # Mark global max (green) and min (red) found by brute-force search
@@ -83,44 +93,6 @@ function main(; nbits = 16, random_seed=1)
   display(plt)
 
   return (;)
-
-  F = Figure(size = (1200, 900))
-  #white = RGBAf(255, 255, 255)
-  #green = RGBf(1/255, 113/255, 0)
-  #blue = RGBf(0, 84/255, 155/255)
-  #red = RGBf(174/255, 42/255, 22/255)
-  #bright_green = RGBf(1/255, 220/255, 0)
-  #bright_red = RGBf(240/255, 0, 0)
-  #ax = Axis(F[1, 1]; xgridcolor=white, ygridcolor=white)
-  #contourf!(ax,x,y,z; colormap=:lajolla)
-
-  linewidth = 0.5
-
-  if show_pivots
-    px = Float64[]
-    py = Float64[]
-    for k in keys(cache)
-      x = b2c(k[1:n])
-      y = b2c(k[n+1:end])
-      push!(px,x)
-      push!(py,y)
-    end
-    scatter!(ax,px,py; color=white, markersize=2.0)
-  end
-  
-  if find_optima
-    Δ = 0.05
-    linewidth = 2.0
-    lines!(ax,[max_x-Δ,max_x+Δ],[max_y ,max_y]; color=bright_green,linewidth)
-    lines!(ax,[max_x,max_x],[max_y-Δ,max_y+Δ]; color=bright_green,linewidth)
-
-    lines!(ax,[min_x-Δ,min_x+Δ],[min_y ,min_y]; color=bright_red,linewidth)
-    lines!(ax,[min_x,min_x],[min_y-Δ,min_y+Δ]; color=bright_red,linewidth)
-  end
-
-  display(F)
-
-  return
 end
 
 #
