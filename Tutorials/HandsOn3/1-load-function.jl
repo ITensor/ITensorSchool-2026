47b431f5409ae654ff3cb8ebb82a5277201b3e60
Miles <miles.stoudenmire@gmail.com>
2026-09-20 21:43:07 -0400

    HO3 Improvements (#24)
    - **Various expansions and improvements to hands on 3**
    - **Simplify some codes**
    - **Improvements to QFT tutorial**
    
    ---------
    
    Co-authored-by: Miles Stoudenmire <mstoudenmire@flatironinstitute.org>

--- Tutorials/HandsOn3/1-load-function.jl
+++ Tutorials/HandsOn3/1-load-function.jl
@@ -1,39 +1,37 @@
 using ITensorMPS: siteinds, maxlinkdim
-using Plots: plot
+using LinearAlgebra: norm
+using Plots: plot, grid
 
 include("resources/tensor_cross/tensor_cross.jl")
-include("resources/integrate.jl")
 include("resources/qtt_utils.jl")
 
 """
-    extract_qtt_values(M::MPS, n::Integer)
+    extract_function_values(M::MPS, m::Integer)
 
-Extract N=2^n values of a function encoded as an
-MPS `M` in the QTT format.
-Returns a vector of N=2^n values.
+Extract 2^m values of a function encoded as an MPS `M`
+in the QTT format, on a coarse grid of 2^m evenly spaced
+points in [0,1). (Fixes all but the first m bits to zero.)
 """
-function extract_function_values(M::MPS, n::Integer)
-    Npoints = 2^n
+function extract_function_values(M::MPS, m::Integer)
     sites = siteinds(M)
-    L = length(M)
-    if n > L-1
-        error("MPS has L=$L indices, maximum n is $(L-1)")
-    end
+    n = length(M)
     R = ITensor(1.)
-    for j=reverse(n+1:L)
+    for j=reverse(m+1:n)
         R *= M[j]*ITensor([1,0],sites[j])
     end
-    T = prod([M[i] for i=1:n])*R
-    A = Array(T,reverse(sites[1:n])...)
-    vals = reshape(A,2^n)
-    return vals
+    T = prod([M[i] for i=1:m])*R
+    A = Array(T,reverse(sites[1:m])...)
+    return reshape(A,2^m)
 end
 
 function main(;
-              n = 32,          # number of bits used to encode function
-              a = 100,         # frequency you can adjust
-              W = 1E-2,        # width of peak
-              log_npoints = 10 # number of grid points for plotting
+              n = 32,           # number of bits used to encode function
+              a = 100,          # frequency you can adjust
+              W = 1E-2,         # width of peak
+              log_npoints = 10, # number of grid points for plotting
+              # Tensor cross parameters
+              nsweep = 5,
+              cutoff = 1E-10,
              )
 
   # Function to be loaded
@@ -45,8 +43,8 @@ function main(;
   println("Performing tensor cross interpolation:")
   M, info = tensor_cross(siteinds("Qubit",n),
                         (bits...)->f(b2c(bits...));
-                         nsweep=5, 
-                         cutoff=1E-10, 
+                         nsweep, 
+                         cutoff, 
                          outputlevel=1)
 
   println()
@@ -57,12 +55,29 @@ function main(;
   println("Max rank χ=$χ")
 
   vals = extract_function_values(M,log_npoints)
-  display(plot_grid_function(vals))
+
+  Δ = 2.0^(-log_npoints)
+  extracted_grid_points = 0:Δ:(1-Δ)
+  exact_vals = f.(extracted_grid_points)
+  @printf("\nError on extracted (plotted) points = %.4E\n",norm(vals-exact_vals))
+
+  display(plot_grid_function(extracted_grid_points, vals, exact_vals))
 
   res = (;)
   return res
 end
 
-function plot_grid_function(vals)
-  return plot(vals; marker=:circle, markersize=2, markercolor=:blue, markerstrokecolor=:blue)
+function plot_grid_function(x, vals, exact_vals)
+  plt_f = plot(x, vals; 
+               marker=:circle, markersize=2, markercolor=:blue, markerstrokecolor=:blue,
+               xlabel="x", ylabel="f(x)", label="")
+
+  # Error versus the true function values. Use a small
+  # floor value so that zero error can be shown on a log scale
+  err = max.(abs.(vals-exact_vals), 1E-16)
+  plt_err = plot(x, err; 
+                 color=:red, yscale=:log10,
+                 xlabel="x", ylabel="error", label="")
+
+  return plot(plt_f, plt_err; layout=grid(2,1,heights=[2/3,1/3]), size=(700,650))
 end
